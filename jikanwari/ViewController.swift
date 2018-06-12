24c3d08654b6fc00170887659579213c2f76090d
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-06-12 21:52:17 +0900

    first commit

---
+++ jikanwari/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jikanwari
+//
+//  Created by Kohei Adachi on 2018/06/08.
+//  Copyright © 2018年 Kohei Adachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ jikanwariTests/jikanwariTests.swift
@@ -0,0 +1,36 @@
+//
+//  jikanwariTests.swift
+//  jikanwariTests
+//
+//  Created by Kohei Adachi on 2018/06/08.
+//  Copyright © 2018年 Kohei Adachi. All rights reserved.
+//
+
+import XCTest
+@testable import jikanwari
+
+class jikanwariTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
